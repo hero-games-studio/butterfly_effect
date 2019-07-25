da8e2d2a231eeab2a6e897753cbf46cb1a39a860
emirhansenkal <46650883+emirhansenkal@users.noreply.github.com>
2019-07-25 11:17:58 +0300

    Last Swipe Design

---
+++ Assets/Scripts/Swipe/SwipeManager.cs
@@ -0,0 +1,55 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwipeManager : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (SwipeInput.Instance.DoubleTap)
+            OnDoubleTap();
+        if (SwipeInput.Instance.Tap)
+            OnTap();
+        if (SwipeInput.Instance.SwipeUp)
+            OnSwipeUp();
+        if (SwipeInput.Instance.SwipeDown)
+            OnSwipeDown();
+        if (SwipeInput.Instance.SwipeLeft)
+            OnSwipeLeft();
+        if (SwipeInput.Instance.SwipeRight)
+            OnSwipeRight();
+    }
+
+    void OnDoubleTap()
+    {
+        Debug.Log("DoubleTap");
+    }
+
+    void OnTap()
+    {
+        Debug.Log("Tap");
+    }
+
+    void OnSwipeUp()
+    {
+        Debug.Log("SwipeUp");
+    }
+
+    void OnSwipeDown()
+    {
+        Debug.Log("SwipeDown");
+    }
+
+    void OnSwipeLeft()
+    {
+        Debug.Log("SwipeLeft");
+    }
+
+    void OnSwipeRight()
+    {
+        Debug.Log("SwipeRigth");
+    }
+}
